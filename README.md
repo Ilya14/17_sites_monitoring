3c3cf7d4c4b6391ef1d5cfe3229375fcfb569cc2
iBogdanov <ilya.bogdanov14@gmail.com>
2016-12-30 20:19:20 +0300

    Скрипт для мониторинга сайтов

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # 17_sites_monitoring
+
+## Описание
+
+Скрипт для проверки состояния сайтов. Статус каждого сайта определяется
+по результатам следующих проверок:
+
+* сервер отвечает на запрос статусом HTTP 200;
+* доменное имя сайта проплачено как минимум на 1 месяц вперед.
+
+## Использование
+Скрипт имеет следующие обязательные параметры:
+
+* url_file - текстовый файл с URL адресами для проверки.
+
+Скрипт имеет следующие опциональные параметры:
+
+* -h, --help - помощь
+
+
+## Пример
+
+Отображение справки:
+
+```sh
+$ python3.5 ./check_sites_health.py -h
+usage: check_sites_health.py [-h] url_file
+
+Script for sites monitoring
+
+positional arguments:
+  url_file    Text file with URL addresses for check
+
+optional arguments:
+  -h, --help  show this help message and exit
+
+```
+
+Пример использования:
+
+```sh
+$ python3.5 ./check_sites_health.py ./sites.txt
+1 URL: http://www.bmstu.ru/; STATUS 200: OK; EXPIRATION DATE: 2017-10-01 (VALID: OK)
+2 URL: https://ru.wikipedia.org; STATUS 200: OK; EXPIRATION DATE: 2023-01-13 (VALID: OK)
+3 URL: https://vk.com; STATUS 200: OK; EXPIRATION DATE: 2017-06-23 (VALID: OK)
+4 URL: http://www.translate.ru/; STATUS 200: OK; EXPIRATION DATE: 2017-04-01 (VALID: OK)
+```
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,56 @@
+import requests
+import whois
+import datetime
+import argparse
+
+
 def load_urls4check(path):
-    pass
+    try:
+        with open(path) as file:
+            for url in file.readlines():
+                yield url.strip()
+    except FileNotFoundError:
+        print('File "{0}" is not found.'.format(path))
+
 
 def is_server_respond_with_200(url):
-    pass
+    status_code = requests.get(url).status_code
+    return True if status_code == 200 else False
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    whois_domain = whois.whois(domain_name)
+    expiration_date = whois_domain.expiration_date
+    return expiration_date.date()
+
+
+def is_domain_expiration_date_valid(expiration_date):
+    days_count = 30
+    today = datetime.date.today()
+    month = datetime.timedelta(days=days_count)
+    month_ahead = today + month
+    return True if expiration_date >= month_ahead else False
+
+
+def get_input_data():
+    parser = argparse.ArgumentParser(description='Script for sites monitoring')
+    parser.add_argument('url_file', help='Text file with URL addresses for check')
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    pass
+    args = get_input_data()
+
+    urls = list(load_urls4check(args.url_file))
+
+    for num, url in enumerate(urls):
+        expiration_date = get_domain_expiration_date(url)
+        print(
+            '{0} URL: {1}; STATUS 200: {2}; EXPIRATION DATE: {3} (VALID: {4})'.format(
+                num + 1,
+                url,
+                'OK' if is_server_respond_with_200(url) else 'NO',
+                expiration_date,
+                'OK' if is_domain_expiration_date_valid(expiration_date) else 'NO',
+            )
+        )
